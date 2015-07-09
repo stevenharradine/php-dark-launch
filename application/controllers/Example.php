5af6587dda69fe09c598cec635ede3c3dc2c3826
Ben Visser <theodore.r.visser@gmail.com>
2015-07-09 10:14:07 -0700

    deleted codeigniter specific code

--- application/controllers/Example.php
+++ application/controllers/Example.php
@@ -5,14 +5,16 @@ class Example extends CI_Controller
   public function __construct()
   {    
     parent::__construct();
-    $this->load->library('dark_launch');
   }
 
 
   public function index()
   {
-    if($this->dark_launch->feature_enabled('time_example')){
-      // code to execute if feature is enabled
-    }
+    $redis = new Redis()
+    $redis->connect('127.0.0.1');
+    $this->load->config('dark_launch');
+    $config = $this->config->items('dark_launch_features')
+    $params = ['redis' => $redis, 'config' => $config]
+    $dark_launch = new Dark_Launch($params);
   }
 }
